0d13a0849ecbfd811fedf9e9faf6443751453a69
Eman <amirm3607@gmail.com>
2024-06-15 10:07:14 -0700

    make view of index

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center align-items-center min-vh-100">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-center text-primary">Todo Application</h1>
+                        <form action="">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <input type="text" name="task" id="task" class="form-cotrol">
+                                </div>
+                                <div class="col-lg-12">
+                                    <input type="submit" name="submit" id="submit" class="btn btn-lg btn-success">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
