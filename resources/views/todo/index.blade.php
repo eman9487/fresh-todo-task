45c147f210f33e4c792fcf03626ba7b3e3ffc04e
Eman <amirm3607@gmail.com>
2024-06-15 23:43:39 -0700

    design in todo folder

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -8,6 +8,15 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        table {
+            border: 2px solid black;
+        }
+        .input{
+            border-color: black;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -20,13 +29,27 @@
                         <form action="">
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <input type="text" name="task" id="task" class="form-cotrol">
+                                    <input type="text" name="task" id="task" class="form-control mt-4 input ">
                                 </div>
                                 <div class="col-lg-12">
-                                    <input type="submit" name="submit" id="submit" class="btn btn-lg btn-success">
+                                    <input type="submit" name="submit" id="submit" class="btn btn-lg btn-primary mt-3">
                                 </div>
                             </div>
                         </form>
+                        <table class="table mt-4 shadow">
+                            <tr>
+                                <th>Sr#</th>
+                                <th>Task</th>
+                                <th>Date</th>
+                                <th>Delete</th>
+                            </tr>
+                            <tr class="text-align-center">
+                                <td>1</td>
+                                <td>Shopping pa jana ha</td>
+                                <td>01-3-2024</td>
+                                <td><button type="submit" name="delete" class="btn btn-md btn-danger">Delete</button></td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
