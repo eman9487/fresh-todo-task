0dbe0eb4033b950968fd182bc8346713b51ea525
Eman <amirm3607@gmail.com>
2024-06-22 23:28:55 -0700

    make route of resource

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/task', TaskController::class);
+Route::resource('task', TaskController::class);
